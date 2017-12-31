697751219eb72f8a0c108a4afdcc04dcc76d99ff
nijel8 <nijel8@gmail.com>
2017-12-31 15:23:15 -0500

    init: set all system properties with device code name

--- init/init_hydrolium.cpp
+++ init/init_hydrolium.cpp
@@ -38,6 +38,24 @@
 
 using android::base::Trim;
 
+static std::string build_code_name;
+
+std::string property_get(const std::string& key)
+{
+  const std::string default_value = "";
+  const prop_info* pi = __system_property_find(key.c_str());
+
+  if (pi == nullptr) return default_value;
+
+  char buf[PROP_VALUE_MAX];
+  if (__system_property_read(pi, nullptr, buf) > 0) return buf;
+
+  // If the property exists but is empty, also return the default value.
+  // Since we can't remove system properties, "empty" is traditionally
+  // the same as "missing" (this was true for cutils' property_get).
+  return default_value;
+}
+
 void property_override(char const prop[], char const value[])
 {
     prop_info *pi;
@@ -49,11 +67,28 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void code_name_property_override(const std::string& key,
+                               const std::string& code_name)
+{
+    std::string value = property_get(key);
+    size_t pos = value.find(build_code_name);
+
+    if (value != "" && pos != value.npos) {
+        while(pos != value.npos) {
+            value.erase(pos, build_code_name.size());
+            value.insert(pos, code_name);
+            pos = value.find(build_code_name);
+        }
+        property_override(key.c_str(), value.c_str());
+    }
+}
+
 void vendor_load_properties()
 {
+    build_code_name = property_get("ro.build.product");
     const char *soc_id_file = "/sys/devices/soc0/soc_id";
-    const char *code_name = "hydrolium";
     std::string soc_id = "-1";
+    std::string code_name = build_code_name;
 
     /* get raw SOC_ID */
     if (read_file(soc_id_file, &soc_id)) {
@@ -63,13 +98,23 @@ void vendor_load_properties()
             code_name = "helium";
         } else {
             ERROR("init_hydrolium: unexpected soc_id = '%s'\n", soc_id.c_str());
+            return;
         }
     } else {
         ERROR("init_hydrolium: failed to read soc_id\n");
+        return;
     }
 
-    INFO("init_hydrolium: soc_id = '%s' => code_name = '%s'\n", soc_id.c_str(), code_name);
-    property_override("ro.product.device", code_name);
-    property_override("ro.build.product", code_name);
+    NOTICE("init_hydrolium: Device SOC_ID is %s", soc_id.c_str());
+    NOTICE("init_hydrolium: Setting up system properties for %s...\n", code_name.c_str());
+
+    property_override("ro.product.device", code_name.c_str());
+    property_override("ro.build.product", code_name.c_str());
+    property_override("ro.cm.device", code_name.c_str());
+    property_override("ro.product.name", code_name.c_str());
+    code_name_property_override("ro.build.fingerprint", code_name);
+    code_name_property_override("ro.build.flavor", code_name);
+    code_name_property_override("ro.build.description", code_name);
+    code_name_property_override("ro.build.display.id", code_name);
 }
 
