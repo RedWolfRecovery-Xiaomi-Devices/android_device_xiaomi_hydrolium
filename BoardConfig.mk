8a42373507f93da937d4056729e312007a7f18c1
nijel8 <nijel8@gmail.com>
2018-01-03 20:36:03 -0500

    BoardCongig: Cleanup and more...
    *use clang
    *replace busybox with toybox - 'tar: Broken pipe' fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@
 
 DEVICE_TREE := device/xiaomi/hydrolium
 
-#USE_CLANG_PLATFORM_BUILD := true
+USE_CLANG_PLATFORM_BUILD := true
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
@@ -41,20 +41,13 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-#TARGET_CPU_VARIANT := cortex-a53
-#TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-#TARGET_2ND_CPU_VARIANT := cortex-a53.a57
-
-# Flags
-#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
@@ -67,17 +60,10 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/hydrolium/mkbootimg.mk
-
-#TARGET_KERNEL_ARCH := arm64
-#TARGET_KERNEL_HEADER_ARCH := arm64
-
-#TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
-
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk ramoops_memreserve=4M androidboot.selinux=permissive
-#BOARD_KERNEL_BASE := 0x80000000
-#BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+
+# Build custom recovery.img or ramdisk will not fit 2/16GB hydrogen
+BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/hydrolium/mkbootimg.mk
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
@@ -93,7 +79,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP
-#TW_USE_TOOLBOX := true
 RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
@@ -105,12 +90,12 @@ TW_DEFAULT_BRIGHTNESS := 80
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_IGNORE_MISC_WIPE_DATA := true
 
-# Device version
+# Use toybox insted of busybox(broken)
+TW_USE_TOOLBOX := true
+
+# My device version
 TW_DEVICE_VERSION := 3
 
-# nijel8@xda BEGIN
-# Blacklist these "input" devices, use TW_INPUT_BLACKLIST := "accelerometer\x0atest1\x0atest2" using the \x0a as a separator between input devices
-# Disable virtual mouse(hide mouse pointer)
 TW_INPUT_BLACKLIST := "pwm-ir"
 TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_NTFS_3G := true
@@ -124,19 +109,9 @@ TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_hydrolium
 TW_INCLUDE_CRYPTO := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
-# Exclude TWRP app
-TW_EXCLUDE_TWRPAPP := true
-
 # Use system vold for decryption
 #TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
 #TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
 
-# Debug flags
-#TWRP_INCLUDE_LOGCAT := true
-#TARGET_USES_LOGD := true
-
-TW_USE_TOOLBOX := false
-
-# For testing
-#TWRP_EVENT_LOGGING := true
+# Exclude TWRP app
+TW_EXCLUDE_TWRPAPP := true
