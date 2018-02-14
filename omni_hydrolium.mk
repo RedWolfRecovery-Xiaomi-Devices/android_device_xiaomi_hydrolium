f3043a7b90e3a1dcdca5de0f1b538e63347c63a1
yshalsager <ysh-alsager@hotmail.com>
2018-02-14 20:42:33 +0200

    adapt for omni source

--- omni_hydrolium.mk
+++ omni_hydrolium.mk
@@ -1,11 +1,11 @@
 #
-# Copyright 2016 The Android Open Source Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +14,20 @@
 # limitations under the License.
 #
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from CM custom product configuration
-$(call inherit-product, vendor/cm/config/common.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hydrolium
+PRODUCT_NAME := omni_hydrolium
 PRODUCT_DEVICE := hydrolium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
-add_lunch_combo lineage_hydrolium-eng
+add_lunch_combo omni_hydrolium-eng
+add_lunch_combo omni_hydrolium-userdebug
